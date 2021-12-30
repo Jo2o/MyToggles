58c1ee3b87234edf8887c59ab84bbae0083ea17f
Jozef Mikuš <Jozef.Mikus@ocean.ibm.com>
2021-12-30 14:29:55 +0100

    -- init

---
+++ src/test/java/com/example/mytoggles/MyTogglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytoggles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTogglesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
