b5a2be6ae73af482351b94c91a01d8128368221e
JozefMikus <jozo.mikus@gmail.com>
2021-12-31 14:13:14 +0100

    -- other gradle minitests

--- src/main/java/com/example/mytoggles/togglz/TogglzController.java
+++ src/main/java/com/example/mytoggles/togglz/TogglzController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.togglz.core.manager.FeatureManager;
-import org.togglz.core.repository.FeatureState;
 
 @Controller
 @Slf4j
