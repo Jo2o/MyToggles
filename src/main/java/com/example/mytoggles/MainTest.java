ddea029e4f31ece049c93dd19034fb92ed84f746
JozefMikus <jozo.mikus@gmail.com>
2022-01-03 23:25:34 +0100

    -- application plugin mini test

---
+++ src/main/java/com/example/mytoggles/MainTest.java
@@ -0,0 +1,11 @@
+package com.example.mytoggles;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class MainTest {
+
+    public static void main(String[] args) {
+        log.info(">>>>>> Inside MainTest.main() <<<<<<<");
+    }
+}
